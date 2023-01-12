be870d08022d44eb97478e76975d237af20878a4
Jack Baron <jackmbaron@gmail.com>
2023-01-12 19:28:11 +0000

    remove hardcoded version

--- scripts/get_tag.sh
+++ scripts/get_tag.sh
@@ -8,9 +8,6 @@ popd () {
   command popd "$@" > /dev/null
 }
 
-# TODO: Remove hardcoded version
-echo "v0.66.3"
-
-# pushd outline
-# echo `git describe --tags $(git rev-list --tags --max-count=1)`
-# popd
+pushd outline
+echo `git describe --tags $(git rev-list --tags --max-count=1)`
+popd
